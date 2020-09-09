cba4bf438f2052f6151b5982a394cb775c64439c
Gabriel Mouallem <gabriel.unifei2017@gmail.com>
2020-09-09 20:50:08 -0300

    minor adjustments

--- client.py
+++ client.py
@@ -1,17 +1,10 @@
 from xmlrpc.client import ServerProxy
 
-server = ServerProxy("http://189.84.197.211:8000", allow_none=True)
+server = ServerProxy("http://ff9c2de17b3a.ngrok.io:8000", allow_none=True)
 
-response = server.tuple_space_out(("teste", 1))
-print(response)
-response = server.tuple_space_out(("teste2", 2))
-print(response)
-response = server.tuple_space_out(("teste3", 3))
-print(response)
-response = server.tuple_space_rt(("teste2", 2))
-print(response)
-response = server.tuple_space_all()
-print(response)
+temp_tuple = ("teste", 1, 2)
+
+print(server.write(temp_tuple))
 
 # response = server.tuple_space_rd()
 # print(response)
--- server.py
+++ server.py
@@ -1,69 +1,38 @@
 from xmlrpc.server import SimpleXMLRPCServer
-import SimpleTS
+from tuplespace import TupleSpace
 
 
 class Server:
     _server_methods = [
-        'hello_world',
-        'tuple_space_rt',
-        'tuple_space_rd',
-        'tuple_space_out',
-        'tuple_space_all',
-        'tuple_space_ref_in',
-        'tuple_space_ref_rd',
-        'tuple_space_ref_out',
-        'tuple_space_ref_eval',
-        'provide_tuple_space',
+        'write',
+        'read',
+        'take',
+        'getAllTuples',
     ]
 
     def __init__(self, address):
         self._server = SimpleXMLRPCServer(address, allow_none=True)
-        self.tuple_space = SimpleTS.TupleSpace()
+        self.tuple_space = TupleSpace()
 
         for method in self._server_methods:
             self._server.register_function(getattr(self, method))
 
-    def hello_world(self):
-        return "Hello World"
+    def write(self, t):
+        return self.tuple_space.write(t)
 
-    def tuple_space_rt(self, template=()):
-        return (str(self.tuple_space.rt(template)))
+    def read(self, t):
+        return self.tuple_space.read(t)
 
-    def tuple_space_rd(self, template=()):
-        return "teste" + str(self.tuple_space.rd(template))
+    def take(self, t):
+        return self.tuple_space.take(t)
 
-    def tuple_space_out(self, t):
-        try:
-            self.tuple_space.out(t)
-            return t
-        except Exception as e:
-            return str(e)
-
-    def tuple_space_all(self):
-        return self.tuple_space.all()
-
-    def tuple_space_ref_in(self, ts, template=()):
-        SimpleTS.TupleSpaceRef(ts).rt(template)
-
-    def tuple_space_ref_rd(self, ts, template=()):
-        SimpleTS.TupleSpaceRef(ts).rd(template)
-
-    def tuple_space_ref_out(self, ts, t):
-        SimpleTS.TupleSpaceRef(ts).out(t)
-
-    def tuple_space_ref_eval(self, ts, pre_tuple, func, args, post_tuple=()):
-        SimpleTS.TupleSpaceRef(ts).eval(pre_tuple, func, args, post_tuple)
-
-    def provide_tuple_space(self, ts):
-        """Host a named tuple space in this process"""
-        ts = SimpleTS.TupleSpace()
-        # pyrocomm.provide_object(ts, _tsName2PyroName(tsName))
-        SimpleTS.TupleSpaceRef(ts)
+    def getAllTuples(self):
+        self.tuple_space.getAllTuples()
 
     def init_server(self):
         self._server.serve_forever()
 
 if __name__ == '__main__':
-    server = Server(('', 8000))
+    server = Server(('127.0.0.1', 8000))
     print("Server started.")
     server.init_server()
